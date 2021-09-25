097d5ce25d12965ec3c4784ac468a1476146da3e
ercodr <82173838+Dahcodingkid@users.noreply.github.com>
2021-09-25 13:44:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const display = document.getElementById('scr-txt');
+const nameField = document.getElementById('search-input');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    guess();
+
+});
+
+async function guess(params) {
+    fetch(`https://api.genderize.io?name=${nameField.value}`)
+
+    .then( res => res.json())
+
+    .then( value => {
+
+        if( parseFloat(value.probability) <= 0.6){
+            display.textContent = `Haven't heard of ${value}, Chances you're either a "male" or a "female" is ${ parseFloat(value.probability)*100 }%`    
+        } else {
+            display.textContent = `Chances you're a ${value.gender} is ${ parseFloat(value.probability)*100 }%`
+        } 
+
+    })
+    
+    .catch( error => {
+        
+        display.innerHTML = `${error} <br><br> <h6>check your internet connection!</h6>`;
+    
+    })
+}
